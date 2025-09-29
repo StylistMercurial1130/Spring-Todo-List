932fbf1d4c2067f84ab0ba263d3d755886733567
StylistMercurial1130 <wrk.krthik@gmail.com>
2025-09-29 10:24:29 +0530

    adding user login service

--- todoapp/src/main/java/com/personal/todoapp/services/Auth/AuthService.java
+++ todoapp/src/main/java/com/personal/todoapp/services/Auth/AuthService.java
@@ -7,6 +7,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 public class AuthService {
     @Value("${auth.jwt.secret}")
     private String token;
+    
     private Algorithm algorithm;
 
     public AuthService() {
